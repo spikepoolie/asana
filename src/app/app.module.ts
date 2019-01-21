6f947de9f02f31d5adc919ca73dfaa6e9aa59a51
Rodrigo Schreiner <clintscoob@hotmail.com>
2019-01-21 12:35:21 -0800

    geolocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { AuthGuard } from './auth.guard';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA, Component } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavabarComponent } from '../app/components/navabar/navabar.component';
@@ -11,6 +11,8 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
 import { DeferLoadModule } from '@trademe/ng-defer-load';
 import { PetDeailsComponent } from './components/pet-deails/pet-deails.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
+
 
 
 @NgModule({
@@ -24,8 +26,11 @@ import { AdminComponent } from './components/admin/admin.component';
   ],
   imports: [
     BrowserModule,
+    AngularMultiSelectModule,
+    FormsModule,
     AppRoutingModule,
     DeferLoadModule,
+
     MDBBootstrapModule.forRoot()
   ],
   schemas: [NO_ERRORS_SCHEMA],
--- src/app/components/pets/pets.component.ts
+++ src/app/components/pets/pets.component.ts
@@ -1,6 +1,6 @@
 import { JSDocTagName } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ModalDirective } from 'angular-bootstrap-md';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
 import DogsJson from '../../../assets/data/dogs.json';
 @Component({
   selector: 'app-pets',
@@ -8,22 +8,21 @@ import DogsJson from '../../../assets/data/dogs.json';
   styleUrls: ['./pets.component.scss']
 })
 
-
 export class PetsComponent implements OnInit {
   dogsJson = [];
   dogImageUrl = '';
   dogsImageUrl = [];
   dogName = '';
+  address = '';
+  geoLocationErrorMessage = '';
+  nearByLocation;
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
 
-
-  @ViewChild('basicModal') public basicModal: ModalDirective;
-  public isModalShown = false;
 
   constructor() {
     this.dogsJson = DogsJson.dogs;
-    // this.dogsJson.forEach(element => {
-    //   console.log(element);
-    // });
     for (let i = 0; i < this.dogsJson.length; i++) {
       const url = this.dogsJson[i].image;
       this.dogsImageUrl[i] = {
@@ -38,22 +37,107 @@ export class PetsComponent implements OnInit {
     }
    }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getLocation();
 
-  showDogModal(img, dognameselected) {
+    this.dropdownList = [
+      {'id': 1, 'itemName': 'India'},
+      {'id': 2, 'itemName': 'Singapore'},
+      {'id': 3, 'itemName': 'Australia'},
+      {'id': 4, 'itemName': 'Canada'},
+      {'id': 5, 'itemName': 'South Korea'},
+      {'id': 6, 'itemName': 'Germany'},
+      {'id': 7, 'itemName': 'France'},
+      {'id': 8, 'itemName': 'Russia'},
+      {'id': 9, 'itemName': 'Italy'},
+      {'id': 10 ,'itemName': 'Sweden'}
+    ];
+  this.selectedItems = [
+        {'id': 2, 'itemName': 'Singapore'},
+        {'id': 3, 'itemName': 'Australia'},
+        {'id': 4, 'itemName': 'Canada'},
+        {'id': 5, 'itemName': 'South Korea'}
+    ];
+
+  this.dropdownSettings = {
+          singleSelection: false,
+          text: 'Select Countries',
+          selectAllText: 'Select All',
+          unSelectAllText: 'UnSelect All',
+          enableSearchFilter: true,
+          classes: 'pets'
+        };
+  }
+
+  onItemSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  OnItemDeSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+  onDeSelectAll(items: any) {
+    console.log(items);
+  }
+
+  showDogModal = (img, dognameselected) => {
     this.dogImageUrl = img;
     this.dogName = dognameselected;
     document.getElementById('modal01').style.display = 'block';
-
   }
 
-  trackByFn(index, item) {
-    return index; // or item.id
+  trackByFn = (index, item) => index;
+
+    HideModal = () => document.getElementById('modal01').style.display = 'none';
+
+   getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.showMe, this.showError);
+    } else {
+      this.geoLocationErrorMessage = 'Geolocation is not supported by this browser.';
+    }
   }
 
- HideModal() {
-   document.getElementById('modal01').style.display = 'none';
- }
+  showMe = (position) => {
+    const a = this.showPosition(position).then((json) => {
+      this.nearByLocation = json;
+    });
+  }
 
+  showPosition = (position) => {
 
+    const latlng = { lat: position.coords.latitude, lng: position.coords.longitude};
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+    return fetch(
+      'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&sensor=true&key=AIzaSyBipOsDkqD6koSbj2nEyMJjbx1u-hy1T_I'
+    )
+      .then(response => response.json())
+      .then((p) => {
+        console.log(p.results[0].address_components[6].long_name);
+        const nearByLocaion = `${p.results[0].address_components[2].long_name} - ${p.results[0].address_components[6].long_name}`;
+        return nearByLocaion;
+      });
+  }
+
+  showError = (error) => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        this.geoLocationErrorMessage = 'User denied the request for Geolocation.';
+        break;
+      case error.POSITION_UNAVAILABLE:
+        this.geoLocationErrorMessage = 'Location information is unavailable.';
+        break;
+      case error.TIMEOUT:
+        this.geoLocationErrorMessage = 'The request to get user location timed out.';
+        break;
+      case error.UNKNOWN_ERROR:
+        this.geoLocationErrorMessage = 'An unknown error occurred.';
+        break;
+    }
+  }
 }
